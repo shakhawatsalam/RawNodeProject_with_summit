5a8dde4f74b2d17ba0d57edff35386da50bc5f55
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-09-27 21:44:17 +0600

    working on first lession

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // dependencies
 
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - modulescaffolding
 const app = {};
@@ -16,12 +18,30 @@ app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
         console.log(`listening to port number ${app.config.port}`);
-        
+
     });
 };
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
+    // request handling
+    // get the url and parse it 
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const hadersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    req.on('data', (buffer) => {
+
+    });
+
+
+
+    console.log(hadersObject.title);
+
     res.end("hello world")
 };
 
